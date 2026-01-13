0dc696188f9d98e5b97c051e25479491cf6f03d3
daneyoh <daney789@gmail.com>
2026-01-13 15:14:41 +0900

    index.html 썸네일 및 경로 최종 수정9

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // 아래 내용을 추가합니다.
-    allowedHosts: [
-      "plain-dryers-hug.loca.lt"
-    ]
-  }
+  // 아래 define 설정이 없으면 옛날 라이브러리들이 process를 찾다가 에러가 납니다.
+  define: {
+    'process.env': {},
+  },
 })
